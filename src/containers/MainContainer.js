b500a263b607f04431d4869d4df059a4bd665402
sylwiavargas <sylwia.vargas@gmail.com>
2020-04-14 21:44:32 -0400

    adds selection methods

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -17,13 +17,30 @@ export default function MainContainer() {
     })
     .catch(console.error)
 
+    // selects all resources (i.e. not countries, not themes, not types)
     const selectResources = () => {
-        return entries.filter((e) => e.sys.contentType.sys.id === "resource")
+        return entries.filter((entry) => entry.sys.contentType.sys.id === "resource")
     }
 
-    let data = selectResources()
+    // returns resources for each page -- it accepts an argument of a string that's the name of the page
+    const selectResourcesForThePage = (str) => {
+        return selectResources().filter((entry) => entry.fields.category.fields.name === str)
+    }
+    // console.log(selectResourcesForThePage("Tools"))
+    // console.log(selectResourcesForThePage("Policy"))
 
-    console.log(data);
+    /////////////// RETURNS ALL AVAILABLE COUNTRIES
+    const selectCountries = () => {
+        return entries.filter((entry) => entry.sys.contentType.sys.id === "country").map((c) => c.fields.name)
+    }
+    // console.log(selectCountries());
+    
+    /////////////// RETURNS ALL AVAILABLE THEMES
+    const selectThemes = () => {
+        return entries.filter((entry) => entry.sys.contentType.sys.id === "name").map((c) => c.fields.name)
+    }
+    // console.log(selectThemes()) 
+    
 
     return (
         <main className='main-container'>
@@ -31,7 +48,7 @@ export default function MainContainer() {
                 <Route exact path="/" component={About} />
                 <Route path="/about" component={About} />
                 <Route path="/principles" component={Principles} />
-                <Route path="/(resources|events|policy-tracker)/" component={(props) => <ItemsContainer {...props} items={data}/>}  />
+                <Route path="/(resources|events|policy-tracker)/" component={(props) => <ItemsContainer {...props} items={selectResources()}/>}  />
                 <Route component={NotFound} />
             </Switch>
         </main>
