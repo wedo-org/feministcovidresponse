96295d5b0f2ed30bda067cbc9963aba7a8c402dc
sylwiavargas <sylwia.vargas@gmail.com>
2020-04-16 10:53:49 -0400

    connects themes and countries to ItemsContainer

--- src/containers/ItemsContainer.js
+++ src/containers/ItemsContainer.js
@@ -2,27 +2,52 @@ import React, { withRouter, useState, useEffect } from 'react'
 import ItemCard from '../components/ItemCard'
 import ButtonsContainer from './ButtonsContainer';
 import { withContentful } from 'react-contentful';
-import { getResourcesForThePage } from '../utils.js'
+import { fetchResourcesForThePage, fetchCountries, fetchThemes } from '../utils.js'
 
 function ItemsContainer(props) {
 
-    // Sylwia: IN THE BELOW FUNCTION WE ALSO NEED TO MAKE A CALL TO AN APPROPRIATE SELECT FUNCTION, e.g. selectResourcesForThePage("Policy") -- let's keep in mind that the sectionTitle is different than what these entries' categories are called on the backend, sadly
+    // clicking on the button selects a country or theme, and that filters entries;
+    // pass down as props updateChosenCountry and updateChosenCategory to the ButtonCont
 
-    const [entries, updateEntries ] = useState([])
-    const [title, displayTitle ] = useState('')
+    const [ entries, updateEntries ] = useState([])
+    const [ title, updateTitle ] = useState('')
+    const [ countries, updateCountries ] = useState([])
+    const [ themes, updateThemes ] = useState([])
+    const [ chosenCountry, updateChosenCountry ] = useState('')
+    const [ chosenCategory, updateChosenCategory ] = useState('')
 
     useEffect( () => {
       sectionTitle()
     }, [])
 
+    useEffect( () => {
+        getCountries()
+      }, [])
+
+      useEffect( () => {
+        getThemes()
+      }, [])
+
     const getEntries = async (location) => {
-      console.log(location);
-      const items = await getResourcesForThePage(location);
-      console.log(items);
+      const items = await fetchResourcesForThePage(location);
       updateEntries(items)
       return items;
     }
 
+    const getCountries = async () => {
+        const countries = await fetchCountries();
+        updateCountries(countries)
+        return countries
+    }
+
+    const getThemes = async () => {
+        const themes = await fetchThemes()
+        updateThemes(themes)
+        return themes
+    }
+
+
+    // Sylwia: IN THE BELOW FUNCTION WE ALSO NEED TO MAKE A CALL TO AN APPROPRIATE SELECT FUNCTION, e.g. selectResourcesForThePage("Policy") -- let's keep in mind that the sectionTitle is different than what these entries' categories are called on the backend, sadly
 
     const sectionTitle = () => {
         let title;
@@ -44,7 +69,7 @@ function ItemsContainer(props) {
                 title = ""
         }
         
-        displayTitle(title)
+        updateTitle(title)
         getEntries(requestTitle || title)
         return title;
     }
@@ -57,7 +82,10 @@ function ItemsContainer(props) {
             <ButtonsContainer/>
             <ul>
                 {
-                    // props.items.map((item)=> <li><ItemCard item={item}/></li>)
+                    entries.map((item)=> 
+                    <li key={item.sys.id}>
+                        <ItemCard item={item}/>
+                    </li>)
                 }
             </ul>
         </section>
--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ const contentfulClient = new ContentfulClient({
   space: process.env.REACT_APP_CONTENTFUL_SPACE
 });
 
-const getEntries = async () => {
+const fetchEntries = async () => {
   try {
     let entries =  await contentfulClient.getEntries()
     return entries.items
@@ -15,33 +15,27 @@ const getEntries = async () => {
 }
 
 // gets all resources (i.e. not countries, not themes, not types)
-const getAllResources = async () => {
-    let entries = await getEntries()
+const fetchAllResources = async () => {
+    let entries = await fetchEntries()
    return entries.filter((entry) => entry.sys.contentType.sys.id === "resource")
 }
 
-// (async () => console.log(await getAllResources()))()
-
 // returns resources for each page -- it accepts an argument of a string that's the name of the page
-const getResourcesForThePage = async (str) => {
-  let entries = await getAllResources()
+const fetchResourcesForThePage = async (str) => {
+  let entries = await fetchAllResources()
     return entries.filter((entry) => entry.fields.category.fields.name === str)
 }
-// console.log(getResourcesForThePage("Tools"))
-// console.log(getResourcesForThePage("Policy"))
 
 /////////////// RETURNS ALL AVAILABLE COUNTRIES
-const getCountries = () => {
-    return getEntries().filter((entry) => entry.sys.contentType.sys.id === "country").map((c) => c.fields.name)
+const fetchCountries = async () => {
+  let entries = await fetchEntries()
+  return entries.filter((entry) => entry.sys.contentType.sys.id === "country").map((c) => c.fields.name)
 }
-// console.log(getCountries());
 
 /////////////// RETURNS ALL AVAILABLE THEMES
-const getThemes = () => {
-    return getEntries().filter((entry) => entry.sys.contentType.sys.id === "name").map((c) => c.fields.name)
+const fetchThemes = async () => {
+  let entries = await fetchEntries()
+  return entries.filter((entry) => entry.sys.contentType.sys.id === "name").map((c) => c.fields.name)
 }
-// console.log(getThemes())
 
-
-
-export { getEntries, contentfulClient, getAllResources, getThemes, getCountries, getResourcesForThePage }
+export { fetchEntries, contentfulClient, fetchAllResources, fetchThemes, fetchCountries, fetchResourcesForThePage }
