062d90e0f1254526f9ac670d53499354e802d3b9
sylwiavargas <sylwia.vargas@gmail.com>
2020-04-27 01:21:22 -0400

    favicon

--- src/components/PrincipleCard.js
+++ src/components/PrincipleCard.js
@@ -8,6 +8,11 @@ export default function PrincipleCard({principle}) {
         updateIsActive(!isActive)
     }
 
+    const makeParagraphs = () => {
+        let paragraphs = principle.paragraphs.split('\n').map(p => p)   
+        return paragraphs;
+    }
+
     return (
         <div className="accordion-item" onClick={()=>toggleClass()}>
             <section className="blob-principle">
@@ -15,15 +20,29 @@ export default function PrincipleCard({principle}) {
               <a className={`${isActive ? "active" : null}`}>{principle.title}</a>
             </section>
             <div className={`content ${isActive ? "active" : null}`}>
-                <p className="principle-description">{principle.description} </p>
-                <ul>
-                    {
-                        principle.items.map((item) =>
-                            <li key={item} className="principle-item">{item}</li>
-                        )
-                    }
-                    </ul>
-
+                {   principle.description 
+                    ?
+                        <p className="principle-description">{principle.description} </p>
+                    :
+                    null
+                }
+                {   principle.items 
+                    ?
+                        <ul>
+                        {   
+                            principle.items.map((item) =>
+                                <li key={item} className="principle-item">{item}</li>
+                            )
+                        }
+                        </ul>
+                    : null
+                }
+                {   principle.paragraphs 
+                    ?
+                makeParagraphs().map(p => <p className="principle-description" key={principle.title}> {p}</p>)
+                    :
+                    null
+                }
             </div>
         </div>
     )
