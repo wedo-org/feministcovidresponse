7930153e9e82efd114e2ffea6d86a48be3ab18b6
luanesouza <luanesouza411@gmail.com>
2020-04-28 19:20:12 -0400

    solves issue of window getting smaller when loading

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import regressive from '../assets/images/mobile-regressive.png';
+import progressive from '../assets/images/mobile-progressive.png';
+import neutral from '../assets/images/mobile-neutral.png';
 
 export default function Legend() {
 
@@ -13,10 +16,12 @@ export default function Legend() {
                 <br/>
                 <li><strong>Law or Policy</strong> - law enacted by a Government in response to COVID.</li>
             </ul>
-            <p className="legend-icons-p">Icons:</p>
-            <img src={require('../assets/images/progressive.png')} className="legend-tracker-doodles" alt="progressive action icon"/><span>Progressive action</span><br/>
-            <img src={require('../assets/images/regressive.png')} className="legend-tracker-doodles" alt="regressive action icon"/><span>Regressive action</span><br/>
-            <img src={require('../assets/images/neutral.png')} className="legend-tracker-doodles" alt="neutral action icon"/><span>Neutral or "too early to tell"</span><br/>
+            <section className='legend-icons'>
+              <p className="legend-icons-p">Icons:</p>
+              <img src={progressive} className="legend-tracker-doodles" alt="progressive action icon"/><span>Progressive action</span><br/>
+              <img src={regressive} className="legend-tracker-doodles" alt="regressive action icon"/><span>Regressive action</span><br/>
+              <img src={neutral} className="legend-tracker-doodles" alt="neutral action icon"/><span>Neutral or "too early to tell"</span><br/>
+            </section>
         </aside>
     )
 }
