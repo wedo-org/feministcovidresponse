e3b03ddf3a6475b12a4c1c95ca1df561a784bc7e
sylwiavargas <sylwia.vargas@gmail.com>
2020-04-30 19:54:49 -0400

    cleanup

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ItemsContainer from './ItemsContainer';
 import About from '../components/About'
