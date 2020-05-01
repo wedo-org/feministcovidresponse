7544d2d8f8b92921721d2047a44c47c2b60d34f4
luanesouza <luanesouza411@gmail.com>
2020-05-01 08:07:39 -0400

    changes images in legend

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -19,9 +19,9 @@ export default function Legend() {
                 <li>{t("Law or Policy")} - {t("law enacted by a Government in response to COVID")}</li>
             </ul>
             <p className="legend-icons-p">{t("ICONS:")}</p>
-            <img src={require('../assets/images/progressive.png')} className="legend-tracker-doodles" alt="progressive action icon"/><span>{t("Progressive action")}</span><br/>
-            <img src={require('../assets/images/regressive.png')} className="legend-tracker-doodles" alt="regressive action icon"/><span>{t("Regressive action")}</span><br/>
-            <img src={require('../assets/images/neutral.png')} className="legend-tracker-doodles" alt="neutral action icon"/><span>{t("Neutral or 'too early to tell'")}</span><br/>
+            <img src={progressive} className="legend-tracker-doodles" alt="progressive action icon"/><span>{t("Progressive action")}</span><br/>
+            <img src={regressive} className="legend-tracker-doodles" alt="regressive action icon"/><span>{t("Regressive action")}</span><br/>
+            <img src={neutral} className="legend-tracker-doodles" alt="neutral action icon"/><span>{t("Neutral or 'too early to tell'")}</span><br/>
 
         </aside>
     )
